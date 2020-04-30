ad8a473efc892db4469627eff65b22a63d91ed6d
Kellie Fontes <kfontes@users.noreply.github.com>
2020-04-29 20:48:14 -0700

    Add files via upload

---
+++ bmtpatterns.js
@@ -0,0 +1,124 @@
+
+var counter = 1;
+
+/* Jabs */
+var jabOPic = "file:///Users/kelliefontes/Documents/BMTPatterns/jab-o.jpeg"
+var jabSPPic = "file:///Users/kelliefontes/Documents/BMTPatterns/jab-sp.jpeg"
+
+function jabPicture(){ 
+
+    document.getElementById('pic' + counter++).src = jabOPic.replace('10x10','10x10');
+    document.getElementById('pic' + counter++).src = jabSPPic.replace('10x10','10x10');
+
+}
+
+/* Crosses */
+var crossOPic = "file:///Users/kelliefontes/Documents/BMTPatterns/cross-o.jpeg"
+var crossSPPic = "file:///Users/kelliefontes/Documents/BMTPatterns/cross-sp.jpeg"
+
+function crossPicture() {
+
+    document.getElementById('pic' + counter++).src = crossOPic.replace('10x10','10x10');
+    document.getElementById('pic' + counter++).src = crossSPPic.replace('10x10','10x10');  
+
+}
+
+/* Left Hooks */
+var leftHookOPic = "file:///Users/kelliefontes/Documents/BMTPatterns/leftHook-o.jpeg"
+var leftHookSPPic = "file:///Users/kelliefontes/Documents/BMTPatterns/leftHook-sp.jpeg"
+
+function lHPicture(){ 
+
+    document.getElementById('pic' + counter++).src = leftHookOPic.replace('10x10','10x10');
+    document.getElementById('pic' + counter++).src = leftHookSPPic.replace('10x10','10x10');
+
+}
+
+/* Right Hooks */
+var rightHookOPic = "file:///Users/kelliefontes/Documents/BMTPatterns/rightHook-o.jpeg"
+var rightHookSPPic = "file:///Users/kelliefontes/Documents/BMTPatterns/rightHook-sp.jpeg"
+
+function rHPicture() {
+
+    document.getElementById('pic' + counter++).src = rightHookOPic.replace('10x10','10x10');
+    document.getElementById('pic' + counter++).src = rightHookSPPic.replace('10x10','10x10');  
+
+}
+
+/* Left Uppercuts */
+var leftUCOPic = "file:///Users/kelliefontes/Documents/BMTPatterns/leftUC-o.jpeg"
+var leftUCSPPic = "file:///Users/kelliefontes/Documents/BMTPatterns/leftUC-sp.jpeg"
+
+function leftUCPicture(){ 
+
+    document.getElementById('pic' + counter++).src = leftUCOPic.replace('10x10','10x10');
+    document.getElementById('pic' + counter++).src = leftUCSPPic.replace('10x10','10x10');
+
+}
+
+/* Right Uppercuts */
+var rightUCOPic = "file:///Users/kelliefontes/Documents/BMTPatterns/rightUC-o.jpeg"
+var rightUCSPPic = "file:///Users/kelliefontes/Documents/BMTPatterns/rightUC-sp.jpeg"
+
+function rightUCPicture() {
+
+    document.getElementById('pic' + counter++).src = rightUCOPic.replace('10x10','10x10');
+    document.getElementById('pic' + counter++).src = rightUCSPPic.replace('10x10','10x10');  
+
+}
+
+/* Right Overhand */
+var rightOHOPic = "file:///Users/kelliefontes/Documents/BMTPatterns/rightOH-o.jpeg"
+var rightOHSPPic = "file:///Users/kelliefontes/Documents/BMTPatterns/rightOH-sp.jpeg"
+
+function rightOHPicture() {
+
+    document.getElementById('pic' + counter++).src = rightOHOPic.replace('10x10','10x10');
+    document.getElementById('pic' + counter++).src = rightOHSPPic.replace('10x10','10x10');  
+
+}
+
+/* Left Slips */
+var leftSOPic = "file:///Users/kelliefontes/Documents/BMTPatterns/leftS-o.jpeg"
+var leftSSPPic = "file:///Users/kelliefontes/Documents/BMTPatterns/leftS-sp.jpeg"
+
+function leftSlipPicture(){ 
+
+    document.getElementById('pic' + counter++).src = leftSOPic.replace('10x10','10x10');
+    document.getElementById('pic' + counter++).src = leftSSPPic.replace('10x10','10x10');
+
+}
+
+/* Right Slips */
+var rightSOPic = "file:///Users/kelliefontes/Documents/BMTPatterns/rightS-o.jpeg"
+var rightSSPPic = "file:///Users/kelliefontes/Documents/BMTPatterns/rightS-sp.jpeg"
+
+function rightSlipPicture() {
+
+    document.getElementById('pic' + counter++).src = rightSOPic.replace('10x10','10x10');
+    document.getElementById('pic' + counter++).src = rightSSPPic.replace('10x10','10x10');  
+
+}
+
+/* Left Rolls */
+var leftRollOPic = "file:///Users/kelliefontes/Documents/BMTPatterns/leftRoll-o.jpeg"
+var leftRollSPPic = "file:///Users/kelliefontes/Documents/BMTPatterns/leftRoll-sp.jpeg"
+
+function leftRollPicture(){ 
+
+    document.getElementById('pic' + counter++).src = leftRollOPic.replace('10x10','10x10');
+    document.getElementById('pic' + counter++).src = leftRollSPPic.replace('10x10','10x10');
+
+}
+
+/* Right Rolls */
+var rightRollOPic = "file:///Users/kelliefontes/Documents/BMTPatterns/rightRoll-o.jpeg"
+var rightRollSPPic = "file:///Users/kelliefontes/Documents/BMTPatterns/rightRoll-sp.jpeg"
+
+function rightRollPicture() {
+
+    document.getElementById('pic' + counter++).src = rightRollOPic.replace('10x10','10x10');
+    document.getElementById('pic' + counter++).src = rightRollSPPic.replace('10x10','10x10');  
+
+}
+
